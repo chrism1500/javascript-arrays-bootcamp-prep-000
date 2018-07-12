c83ed9e17a26bb6d3cfb55008881bfa3d3ab8d8b
morales.chris1989@gmail.com <morales.chris1989@gmail.com>
2018-07-12 14:41:49 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,3 +1,4 @@
 var chocolateBars = [ "snickers", "hundred grand", "kitkat", "skittles"];
-addElementToBeginningOfArray
-destructivelyAddElementToBeginningOfArray
+var addElementToBeginningOfArray;
+var destructivelyAddElementToBeginningOfArray;
+
